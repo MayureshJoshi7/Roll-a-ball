b83703e4f828ef7c6e5f540cb8ea87833ade05f8
Mayuresh <mayureshjoshi2703@gmail.com>
2020-07-06 01:33:25 +0530

    First Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardforce = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(0,200,800);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardforce*Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(500 * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce((-500) * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, 500 * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, (-500) * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
---
+++ Assets/Scripts/Playerscript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerscript : MonoBehaviour
+{
+    public Playerscript movement;
+
+   
+    void OnCollisionEnter(Collision CollisionInfo)
+    {
+        if(CollisionInfo.collider.tag=="obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+
+}
